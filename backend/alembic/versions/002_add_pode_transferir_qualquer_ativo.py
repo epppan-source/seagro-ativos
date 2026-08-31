dafbcedfb470a36705d12aecde0c0780eae24a98
epppan-source <epppan@gmail.com>
2026-08-31 11:58:57 -0300

    Update 002_add_pode_transferir_qualquer_ativo.py

--- backend/alembic/versions/002_add_pode_transferir_qualquer_ativo.py
+++ backend/alembic/versions/002_add_pode_transferir_qualquer_ativo.py
@@ -1,6 +1,6 @@
 """add pode_transferir_qualquer_ativo to funcionarios
 
-Revision ID: 002_add_pode_transferir_qualquer_ativo
+Revision ID: 002_pode_transferir_ativo
 Revises: 001_add_solicitacoes_baixa
 Create Date: 2026-08-31
 
@@ -8,7 +8,7 @@ Create Date: 2026-08-31
 from alembic import op
 import sqlalchemy as sa
 
-revision = "002_add_pode_transferir_qualquer_ativo"
+revision = "002_pode_transferir_ativo"
 down_revision = "001_add_solicitacoes_baixa"
 branch_labels = None
 depends_on = None
